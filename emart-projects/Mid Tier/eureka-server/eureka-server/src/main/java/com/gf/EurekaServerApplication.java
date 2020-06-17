86b4d93bb9b6d2a29aabca6074ca3c8a2773f78f
unknown <ljgliudl@cn.ibm.com>
2020-06-17 17:30:48 +0800

    updated by liujg

--- emart-projects/Mid Tier/eureka-server/eureka-server/src/main/java/com/gf/EurekaServerApplication.java
+++ emart-projects/Mid Tier/eureka-server/eureka-server/src/main/java/com/gf/EurekaServerApplication.java
@@ -1,8 +1,8 @@
 package com.gf;
 
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
